6df05ca7c5acec97f7651479880335bd78abd451
hashiba-k-jp <17132@hakodate.kosen-ac.jp>
2023-05-01 17:02:01 +0900

    hide affi

---
+++ content.js
@@ -0,0 +1,9 @@
+window.addEventListener("load",function() {
+    // const REPLACE_DIV = document.createElement("div");
+    // REPLACE_DIV.innerText = "Here is hided advertisement";
+
+    sponsored = document.querySelector("#tvcap");
+    sponsored.innerHTML = "Here is hided advertisement"
+    sponsored.style.align = "center"
+    // とりあえずこんな感じで広告消せそう？
+});
