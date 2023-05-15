c7817917c15f825f7c96c3cbae92ba0e9f33baa0
hashiba-k-jp <17132@hakodate.kosen-ac.jp>
2023-05-15 17:36:26 +0900

    complete test function: display selected URL.

--- scripts/background.js
+++ scripts/background.js
@@ -1,20 +1,58 @@
-const parent = chrome.contextMenus.create({
-  id: "Affihider",
-  title: "test",
-  contexts: ["all"],
+chrome.runtime.onInstalled.addListener(function(){
+    const parent = chrome.contextMenus.create({
+      id: "AffihiderParent",
+      title: "test",
+      contexts: ["link"]
+    });
+
+    const childSet = chrome.contextMenus.create({
+        title:"normal",
+        id: 'AffihiderSet',
+        parentId: 'AffihiderParent',
+        title: 'set URL',
+        contexts: ["link"]
+    });
+
+    const childUnset = chrome.contextMenus.create({
+        title:"normal",
+        id: 'AffihiderUnset',
+        parentId: 'AffihiderParent',
+        title: 'unset URL',
+        contexts: ["link"]
+    });
 });
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
     switch (info.menuItemId) {
-        case "Affihider":
+        case "AffihiderParent":
+            // do nothing...
+            break;
+        case "AffihiderSet":
             chrome.scripting.executeScript({
-            target: { tabId: tab.id },
-            function: test,
+                target: { tabId: tab.id },
+                function: set,
+                args:[info],
             });
             break;
+        case "AffihiderUnset":
+            chrome.scripting.executeScript({
+                target: { tabId: tab.id },
+                function: unset,
+                args:[info],
+            });
+            break;
+
 }});
 
-function test() {
-    console.log("test done!!!!");
+function set(info){
+    targetUrl = info.linkUrl;
+    console.log("[*] set");
+    console.log(targetUrl);
+}
+
+function unset(info){
+    targetUrl = info.linkUrl;
+    console.log("[*] unset");
+    console.log(targetUrl);
 }
 
